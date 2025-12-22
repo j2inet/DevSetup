632a3a521b6a6a2be603b29886ac99b0ba6566a5
Joel Ivory Johnson <joel@j2i.net>
2025-12-21 20:29:02 -0500

    added build job cap

--- chromium/checkout-chromium-and-build.cmd
+++ chromium/checkout-chromium-and-build.cmd
@@ -1,9 +1,22 @@
 
 ECHO ON
+timeout /t 2100 /nobreak 
 SET drive=c:
 set googlePath=%drive%\shares\projects\google\
 SET VS_EDITION=Community
 SET NINJA_SUMMARIZE_BUILD=1
+SET PARALLEL_JOBS=8
+IF(%PARALLEL_JOBS% EQU 0) (
+    SET JOBS_PARAMETER=-j%PARALLEL_JOBS%    
+) else (
+    SET JOBS_PARAMETER=
+)
+
+
+
+set PATH=%googlePath%depot_tools;%PATH%
+SET DEPOT_TOOLS_WIN_TOOLCHAIN=0
+SET vs2022_install=%drive%\Program Files\Microsoft Visual Studio\18\%VS_EDITION%
 
 git config --global core.longpaths true
 git config --global core.autocrlf false
@@ -17,9 +30,6 @@ mkdir %googlePath%
 cd %googlePath%
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 pushd %googlePath%depot_tools
-set PATH=%googlePath%depot_tools;%PATH%
-SET DEPOT_TOOLS_WIN_TOOLCHAIN=0
-SET vs2022_install=%drive%\Program Files\Microsoft Visual Studio\18\%VS_EDITION%
 
 call gclient
 
@@ -38,10 +48,10 @@ gn gen out\ChromeDebug --args="is_debug=true"
 gn gen out\ChromeRelease --args="is_debug=false symbol_level=1"
 
 
-autoninja -C out\Default base
-autoninja -C out\ChromeDebug chrome
-autoninja -C out\ChromeRelease chrome
-autoninja -C out\v8Debug v8_monolith
-autoninja -C out\v8Release v8_monolith
+autoninja -C out\Default base %JOBS_PARAMETER%
+autoninja -C out\ChromeDebug chrome %JOBS_PARAMETER%
+autoninja -C out\ChromeRelease chrome %JOBS_PARAMETER%
+autoninja -C out\v8Debug v8_monolith %JOBS_PARAMETER%
+autoninja -C out\v8Release v8_monolith %JOBS_PARAMETER%
 
 popd
